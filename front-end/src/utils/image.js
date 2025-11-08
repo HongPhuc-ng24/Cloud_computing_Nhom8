87baa1386e9cdb15fa03047a18240b6846880281
Thái Văn Phúc <phuc100662@donga.edu.vn>
2025-11-08 17:42:04 +0700

    Update image.js

--- front-end/src/utils/image.js
+++ front-end/src/utils/image.js
@@ -1,10 +1,10 @@
 const isProduction = window.location.hostname !== "localhost";
-const urlBackend = isProduction 
-  ? "https://54.166.22.101/"  
-  : "http://localhost:5000/"; 
+const urlBackend = isProduction
+  ? "https://54.166.22.101/"
+  : "http://localhost:5000/";
 
 export const getImageUrl = (imgPath) => {
   if (!imgPath) return "";
-  if (!imgPath.startsWith("/")) imgPath = "/" + imgPath; // đảm bảo có dấu /
+  if (!imgPath.startsWith("/")) imgPath = "/" + imgPath;
   return `${urlBackend}${imgPath}`;
 };
