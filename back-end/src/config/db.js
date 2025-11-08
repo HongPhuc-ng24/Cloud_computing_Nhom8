96ea26cea96cf0523c94ee846f63a886e66f765d
Thái Văn Phúc <phuc100662@donga.edu.vn>
2025-11-08 16:59:26 +0700

    Update db.js

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -4,7 +4,10 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://phuc100662:w5wVbrX6eaMKBsQu@cluster0.kkdpq.mongodb.net/reactjs?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("MongoDB Connected....", conn.connection.host);
   } catch (error) {
     console.error("MongoDB Connection fail...", error);
