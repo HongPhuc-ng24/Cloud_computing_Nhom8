fb398319516a6874dac8a260f22a8f35d7dd68f5
Thái Văn Phúc <phuc100662@donga.edu.vn>
2025-11-12 15:20:42 +0700

    up

--- front-end/src/utils/image.js
+++ front-end/src/utils/image.js
@@ -1,7 +1,7 @@
 const isProduction = window.location.hostname !== "localhost";
 const urlBackend = isProduction
   ? "https://cloud-computing-nhom8.onrender.com"
-  : "http://localhost:5000/";
+  : "http://localhost:5000";
 
 export const getImageUrl = (imgPath) => {
   if (!imgPath) return "";
